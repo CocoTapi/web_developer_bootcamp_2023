999d87792496d9778347b306d557e22ea177c1a4
Chocotapi <shioriyoshida2020@gmail.com>
2023-02-23 16:52:39 -0500

    made a new js file and added conditions

---
+++ Sections/Section_16/Conditional/app.js
@@ -0,0 +1,19 @@
+let random = Math.random();
+if (random < 0.5) {
+    console.log("YOUR NUMBRT IS LESS THAN 0.5!!!");
+} else {
+    console.log("YOUR NUMBER IS GRATER THAN OR ERUAL TO 0.5!!!")
+} 
+console.log(random);
+
+// const age = 89
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!");
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10");
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20");
+// } else {
+//     console.log("You are a senior. You pay $10");
+// } 
