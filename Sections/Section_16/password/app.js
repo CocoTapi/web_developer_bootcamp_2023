180e07fc11acfdf26611603e7ad648767bde96cf
Chocotapi <shioriyoshida2020@gmail.com>
2023-02-23 17:27:08 -0500

    nested 2 conditions

--- Sections/Section_16/password/app.js
+++ Sections/Section_16/password/app.js
@@ -2,17 +2,17 @@ const password = prompt("Please enter a new password");
 
 // Password must be 6+ characters
 if (password.length >=6){
+    // Password cannot include space
+    if (password.indexOf(' ') === -1) {
+        console.log("Good job! No space!");
+    } else {
+        console.log("Password cannot contain spaces!");
+    }
     console.log("Long enough password!");
 } else {
     console.log("Your password is too short! Must be 6+ characters!");
 }
 
-// Password cannot include space
 
 //"hello".indexOf (' ') --> -1
 
-if (password.indexOf(' ') === -1) {
-    console.log("Good job! No space!");
-} else {
-    console.log("Password cannot contain spaces!");
-}
