47ab32c3a4540859fae300ec6ff4a1810e1ef6d2
Chocotapi <shioriyoshida2020@gmail.com>
2023-02-23 22:45:44 -0500

    implemented data valification

---
+++ Sections/Section_16/ticketFee/app.js
@@ -0,0 +1,17 @@
+//0 - 4 free
+//5 - 18 $10
+//19 - 64 $20
+//65+ free
+
+const age = -79;
+
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log("FREE");
+} else if (age >= 5 && age < 19) {
+    console.log("$10");
+} else if (age >= 19 && age < 65) {
+    console.log("$20");
+} else {
+    console.log("INVALID AGE");
+}
+   
