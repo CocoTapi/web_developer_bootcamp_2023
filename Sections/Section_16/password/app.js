753e88cc6ca4f7cc5dd18b71a0e8d810c889e221
Chocotapi <shioriyoshida2020@gmail.com>
2023-02-23 22:26:26 -0500

    combined 2 requirements in one line

--- Sections/Section_16/password/app.js
+++ Sections/Section_16/password/app.js
@@ -1,18 +1,23 @@
 const password = prompt("Please enter a new password");
 
-// Password must be 6+ characters
-if (password.length >=6){
-    // Password cannot include space
-    if (password.indexOf(' ') === -1) {
-        console.log("Good job! No space!");
-    } else {
-        console.log("Password cannot contain spaces!");
-    }
-    console.log("Long enough password!");
+//169
+// // Password must be 6+ characters
+// if (password.length >=6){
+//     // Password cannot include space
+//     if (password.indexOf(' ') === -1) {
+//         console.log("Good job! No space!");
+//     } else {
+//         console.log("Password cannot contain spaces!");
+//     }
+//     console.log("Long enough password!");
+// } else {
+//     console.log("Your password is too short! Must be 6+ characters!");
+// }
+// //"hello".indexOf (' ') --> -1
+
+//170 
+if (password.length >= 6 && password.indexOf(' ') === -1) {
+    console.log("Your password is valid");
 } else {
-    console.log("Your password is too short! Must be 6+ characters!");
+    console.log("Your password is invalid");
 }
-
-
-//"hello".indexOf (' ') --> -1
-
