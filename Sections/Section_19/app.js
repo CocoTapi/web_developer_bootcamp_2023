4da696b3fc30e72e042113acba72fa74520c7792
Chocotapi <shioriyoshida2020@gmail.com>
2023-02-25 23:56:35 -0500

    added lists of numbers with nesting loops

--- Sections/Section_19/app.js
+++ Sections/Section_19/app.js
@@ -8,8 +8,23 @@
 //     console.log(i);
 // }
 
-//Loop over the people array with a for loop, printing out each name in uppercase letters.
-const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
-for (let i = 0; i < people.length; i++){
-    console.log(people[i].toUpperCase());
-}
+// //Loop over the people array with a for loop, printing out each name in uppercase letters.
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+// for (let i = 0; i < people.length; i++){
+//     console.log(people[i].toUpperCase());
+// }
+
+// Nesting loops
+
+//i is:1
+    //j is: 1
+    //j is: 2
+    //j is: 3
+//i is 2 ....
+// for (let i = 1; i <= 10; i++){
+//     console.log(`i is: ${i}`);
+//     for (let j = 1; j < 4; j++){
+//         console.log(`       j is: ${j}`);
+//     }
+// }
+
