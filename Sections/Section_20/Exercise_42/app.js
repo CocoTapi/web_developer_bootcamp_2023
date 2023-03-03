2e0f5cee69e0fad849d2fa7a87f8ec6f996335a7
Chocotapi <shioriavino@gmail.com>
2023-03-02 21:22:47 -0500

    created a function to show the sum in the array

--- Sections/Section_20/Exercise_42/app.js
+++ Sections/Section_20/Exercise_42/app.js
@@ -1,2 +1,10 @@
 // Write a function called sumArray which accepts a single argument: an array of numbers.
 // It should return the sum of all the numbers in the array.
+
+function sumArray(nums){
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i]
+    }
+    return total;
+}
