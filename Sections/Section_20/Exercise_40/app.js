b29b4772829feb11e4dbb749b94fd8b0edb9cfc3
Chocotapi <shioriavino@gmail.com>
2023-03-02 21:13:06 -0500

    created a function to show the last element in an array

---
+++ Sections/Section_20/Exercise_40/app.js
@@ -0,0 +1,14 @@
+//write a function called lastElement which accepts a single array argument.  
+//The function should return the last element of the array (without removing the element). 
+//If the array is empty, the function should return null.
+
+//lastElement([3,5,7]) //7
+//lastElement([1]) //1
+//lastElement([]) //null
+
+function lastElement(array) {
+    if (array.length < 1) {      
+        return null
+    }
+    return array[array.length - 1]
+}
---
+++ Sections/Section_20/Exercise_42/app.js
@@ -0,0 +1,2 @@
+// Write a function called sumArray which accepts a single argument: an array of numbers.
+// It should return the sum of all the numbers in the array.
