8e86ad74f1fc3e802a1c38b3182565f65bec526c
Chocotapi <shioriavino@gmail.com>
2023-03-06 18:23:48 -0500

    practiced functions

--- Sections/Section_21/eggLayingExercise/app.js
+++ Sections/Section_21/eggLayingExercise/app.js
@@ -14,3 +14,63 @@ hen.layAnEgg() // "EGG"
 hen.eggCount // 2
  * 
  */
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg: function() {
+        this.eggCount++; //why does this work?
+        return "EGG";
+    }
+}
+console.log(hen.eggCount) //0
+const egg = hen.layAnEgg() // returns and sets "egg" variable as string "EGG"
+console.log(egg) // "EGG"
+console.log(hen.eggCount) //1
+
+/**************************** */
+
+const henTemplate = {
+    name: 'Change_ME!',
+    eggCount: 0,
+    layAnEgg: function (num) {
+        this.eggCount++;
+        return "EGG";
+    }
+}
+
+const hen1 = new henTemplate();
+const hen2 = new henTemplate();
+console.log(hen1.name) // "Change_ME"
+hen1.name = "Jerry";
+console.log(hen1.name) // "Jerry"
+hen1.layAnEgg(100);
+hen2.layAnEgg(3);
+console.log(hen1.eggCount); //100
+console.log(hen2.eggCount); //3
+
+hen1.eggCount += 100; //why not this? (rhetorical)
+
+//here is why:
+
+hen1.eggCount = -5; //this isnt good, we can;t have negative eggs
+console.log(hen1.eggCount); //-5
+
+//what is the solution then?
+const henTemplate2 = {
+    name: 'Change_ME!',
+    eggCount: 0,
+    layAnEgg: function (num) {
+        if (num <= 0) { 
+            return "NO EGG";
+        }
+        this.eggCount++;
+        return "EGG";
+    }
+}
+
+const hen3 = new henTemplate2;
+hen3.layAnEgg(3);
+console.log(hen3.eggCount); //3
+hen3.layAnEgg(-5);
+console.log(hen3.eggCount); //3
