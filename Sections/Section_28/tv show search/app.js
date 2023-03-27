9ee63703c09ab31ceaa7051098477e485d9fbe22
Chocotapi <shioriavino@gmail.com>
2023-03-27 16:26:12 -0400

    added the feature to delete the currently displayed pictures when you perform a new search for pictures of the body in JavaScript

--- Sections/Section_28/tv show search/app.js
+++ Sections/Section_28/tv show search/app.js
@@ -1,22 +1,28 @@
 const form = document.querySelector('#searchForm');
+const body = document.querySelector('body');
+const imgContainer = document.createElement('section');
+document.body.append(imgContainer);
+
+
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    imgContainer.innerHTML = '';
     const searchTerm = form.elements.query.value
-    const config = {params: { q: searchTerm }}
+    const config = { params: { q: searchTerm } }
     const res = await axios.get('https://api.tvmaze.com/search/shows', config)
-    makeImages(res.data);
+    const shows = res.data;
+    makeImages(shows);
     form.elements.query.value = '';  //don't forget this!
-    
-} )
+})
 
 const makeImages = (shows) => {
+
     for (let result of shows) {
         if (result.show.image) {
             const newImg = document.createElement('img');
             newImg.src = result.show.image.medium;
-            document.body.append(newImg)
+            imgContainer.append(newImg)
         }
-        
     };
 }
