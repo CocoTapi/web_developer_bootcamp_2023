f312acdde147e9a4abf2e44dc73e15ec16ab0067
Chocotapi <shioriavino@gmail.com>
2023-04-04 18:06:32 -0400

    added colors for guess and the error message

--- Sections/Section_32/Language_detector/index.js
+++ Sections/Section_32/Language_detector/index.js
@@ -1,15 +1,16 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 const input = process.argv[2];
 // const input = "こんにちわ. 私の名前はザックです"
 
 try {
     const languageCode = franc(input);
     if (!languageCode || languageCode === 'und') {
-        console.log("Sorry, couldn't figure out! Try with more sample text!");
+        console.log("Sorry, couldn't figure out! Try with more sample text!".red);
     } else if (languageCode !== undefined)  {
         const language = langs.where("2", languageCode);
-        console.log(language.name);
+        console.log(`our best guess is: ${language.name}`.green);
     }
 } catch (error) {
     console.log("Sorry, couldn't figure out! Try with more sample text!");
