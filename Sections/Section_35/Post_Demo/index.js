f2ca161ff7566e279e7f34b87e958d9afa79f435
Chocotapi <shioriavino@gmail.com>
2023-04-11 17:38:07 -0400

    added function to handle JSON and URL encoded data

--- Sections/Section_35/Post_Demo/index.js
+++ Sections/Section_35/Post_Demo/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.get('/tacos', (req, res) => {
     res.send("Get /tacos response");
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty} = req.body;
+    res.send(`OK, here are your ${qty} ${meat}`);
 })
 
 app.listen(3000, () => {
