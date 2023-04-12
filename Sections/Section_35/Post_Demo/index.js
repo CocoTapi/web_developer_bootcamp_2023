139ef106f4afb4ffae1d7db1d05cff315102e801
Chocotapi <shioriavino@gmail.com>
2023-04-11 23:58:38 -0400

    made a new comment form in EJS and connect it to JavaScript with a get and post request

--- Sections/Section_35/Post_Demo/index.js
+++ Sections/Section_35/Post_Demo/index.js
@@ -8,19 +8,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.get('/tacos', (req, res) => {
-    res.send("Get /tacos response");
-})
-
-app.post('/tacos', (req, res) => {
-    const { meat, qty} = req.body;
-    res.send(`OK, here are your ${qty} ${meat}`);
-})
-
-app.listen(3000, () => {
-    console.log("ON PORT 3000");
-})
-
 const comments = [
     {
         username: 'Todd',
@@ -47,3 +34,26 @@ const comments = [
 app.get('/comments', (req, res) => {
     res.render('comments/index', {comments});
 })
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const {username, comment} = req.body;
+    comments.push({username, comment})
+    res.send("IT WORKED");
+})
+
+app.get('/tacos', (req, res) => {
+    res.send("Get /tacos response");
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty} = req.body;
+    res.send(`OK, here are your ${qty} ${meat}`);
+})
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000");
+})
+
