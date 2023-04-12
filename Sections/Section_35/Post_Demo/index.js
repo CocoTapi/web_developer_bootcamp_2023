4329b213a2654055f387b5d73662b04bb83b2b07
Chocotapi <shioriavino@gmail.com>
2023-04-11 23:15:08 -0400

    changed the use() method to the set() to fix the error

--- Sections/Section_35/Post_Demo/index.js
+++ Sections/Section_35/Post_Demo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true}));
