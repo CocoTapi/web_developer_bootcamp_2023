11900278b9c1173a958262d04f49d0ecb0bd21cd
Chocotapi <shioriavino@gmail.com>
2023-04-19 16:23:52 -0400

    added error handling in JavaScript

--- Sections/Section_37/MongooseBasics/index.js
+++ Sections/Section_37/MongooseBasics/index.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp');
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    . then(() => {
+        console.log("CONNECTION OPEN!!");
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!");
+        console.log(err);
+    })
