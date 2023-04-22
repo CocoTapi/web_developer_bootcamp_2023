dbfd1128c1aaee4e2e8ba424a1d34b58478db283
Chocotapi <shioriavino@gmail.com>
2023-04-21 23:16:28 -0400

    chenged a app name

--- Sections/Section_37/MongooseBasics/product.js
+++ Sections/Section_37/MongooseBasics/product.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+mongoose.connect('mongodb://127.0.0.1:27017/productApp')
     .then(() => {
         console.log("CONNECTION OPEN!!");
     })
