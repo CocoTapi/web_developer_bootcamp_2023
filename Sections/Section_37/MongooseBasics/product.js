47dfa30ae657cc894213543c0edb917f468daa3c
Chocotapi <shioriavino@gmail.com>
2023-04-21 23:33:09 -0400

    revised schema and added a new product in js

--- Sections/Section_37/MongooseBasics/product.js
+++ Sections/Section_37/MongooseBasics/product.js
@@ -11,7 +11,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/productApp')
     })
 
     const productSchema = new mongoose.Schema({
-        name: String,
-        price: Number,
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        }
     })
+
+    const Product = mongoose.model('Product', productSchema);
+
+    const bike = new Product ({ name: 'Mountain Bike', price: 599});
+    bike.save()
+        .then(data => {
+            console.log("IT WORKED");
+            console.log("data");
+        })
+        .catch(err => {
+            console.log("OH NO ERROR!");
+            console.log(err);
+        })
+
     
