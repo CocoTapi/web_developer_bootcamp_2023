1b536875e6f34721a9a1914e5bd934ae18078886
Chocotapi <shioriavino@gmail.com>
2023-04-21 23:15:17 -0400

    made a new JS file and added a new schema

---
+++ Sections/Section_37/MongooseBasics/product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => {
+        console.log("CONNECTION OPEN!!");
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!");
+        console.log(err);
+    })
+
+    const productSchema = new mongoose.Schema({
+        name: String,
+        price: Number,
+    })
+    
