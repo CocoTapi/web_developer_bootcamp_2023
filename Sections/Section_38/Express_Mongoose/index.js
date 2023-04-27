8d6a736a71237f6099d7d3e7974dc413b920d519
Chocotapi <shioriavino@gmail.com>
2023-04-27 01:13:58 -0400

    checked if a get request is working

--- Sections/Section_38/Express_Mongoose/index.js
+++ Sections/Section_38/Express_Mongoose/index.js
@@ -5,6 +5,10 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
 
+app.get('/cat', (req, res) => {
+    console.log("MEOW")
+})
+
 app.listen(3000, () => {
     console.log('APP IS LISTENING ON PORT 3000')
 })
