e63005fc1dadee3601bd0c45467552e1914af49b
Chocotapi <shioriavino@gmail.com>
2023-04-26 23:09:31 -0400

    made a new file and setup mongoose

---
+++ Sections/Section_37/MongooseBasics/person.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/productApp')
+    .then(() => {
+        console.log("CONNECTION OPEN!!");
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!");
+        console.log(err);
+    })
+    
