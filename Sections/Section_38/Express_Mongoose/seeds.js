f9e2764f9c2bd6312690cdf2971b50c14fa0901f
Chocotapi <shioriavino@gmail.com>
2023-04-29 15:47:14 -0400

    made a new file and connected with mongoose to add new products

---
+++ Sections/Section_38/Express_Mongoose/seeds.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Product = require('./models/product')
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(() => {
+        console.log("Mongo CONNECTION OPEN!!");
+    })
+    .catch(err => {
+        console.log("OH NO Mongo ERROR!!!");
+        console.log(err);
+    })
