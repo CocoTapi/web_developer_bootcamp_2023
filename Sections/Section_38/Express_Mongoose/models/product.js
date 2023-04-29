38f613b68745fcf2d35a7b9977b37185406fda39
Chocotapi <shioriavino@gmail.com>
2023-04-29 16:48:23 -0400

    fixed missing export

--- Sections/Section_38/Express_Mongoose/models/product.js
+++ Sections/Section_38/Express_Mongoose/models/product.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(() => {
+        console.log("Mongo CONNECTION OPEN!!");
+    })
+    .catch(err => {
+        console.log("OH NO Mongo ERROR!!!");
+        console.log(err);
+    })
+
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -7,7 +17,8 @@ const productSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
     },
     category: {
         type: String,
@@ -17,3 +28,5 @@ const productSchema = new mongoose.Schema({
 })
 
 const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
+//don't forget export!!!!!!!!!!!!!!!!!!!!
