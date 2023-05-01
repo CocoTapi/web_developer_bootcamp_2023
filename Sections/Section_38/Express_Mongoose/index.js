22be42e68e0973d9cd6e0bc8d6ede0961d5a583e
Chocotapi <shioriavino@gmail.com>
2023-04-30 22:21:31 -0400

    made a get request for a detail page

--- Sections/Section_38/Express_Mongoose/index.js
+++ Sections/Section_38/Express_Mongoose/index.js
@@ -27,6 +27,13 @@ app.get('/products', async (req, res) => {
     res.render('products/index.ejs', {products})
 })
 
+app.get('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.send('Detail Page');
+})
+
 app.listen(3000, () => {
     console.log('APP IS LISTENING ON PORT 3000')
 })
