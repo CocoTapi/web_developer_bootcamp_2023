f55663034fdf02a5d3ce2dcb70d5bb91fe324df4
Chocotapi <shioriavino@gmail.com>
2023-04-30 23:35:20 -0400

    resolved a rouding issue by changing the order

--- Sections/Section_38/Express_Mongoose/index.js
+++ Sections/Section_38/Express_Mongoose/index.js
@@ -28,15 +28,15 @@ app.get('/products', async (req, res) => {
     res.render('products/index.ejs', {products})
 })
 
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
 app.get('/products/:id', async (req, res) => {
     const {id} = req.params;
     const product = await Product.findById(id);
     console.log(product);
     res.render('products/show', {product});
-})
-
-app.get('/products/new', (req, res) => {
-    res.render('products/new');
 })
 
 app.post('/products', (req, res) => {
