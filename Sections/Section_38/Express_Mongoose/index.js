5e0130bc7dbfcba45d25c727688a79823abaecc2
Chocotapi <shioriavino@gmail.com>
2023-05-01 23:56:41 -0400

    setup the method-override

--- Sections/Section_38/Express_Mongoose/index.js
+++ Sections/Section_38/Express_Mongoose/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product')
 
@@ -17,7 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/cat', (req, res) => {
     console.log("MEOW")
