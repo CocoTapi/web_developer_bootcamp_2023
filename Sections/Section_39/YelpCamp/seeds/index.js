f8657dc3fb483d4b7b92b7e2aabf73572db0fabf
Chocotapi <shioriavino@gmail.com>
2023-05-11 04:07:11 -0400

    deleted the dupricate

--- Sections/Section_39/YelpCamp/seeds/index.js
+++ Sections/Section_39/YelpCamp/seeds/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('../models/campground');
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
