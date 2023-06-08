0d7447a6b7e51359c8597f5470f86bd873db6e65
Chocotapi <shioriavino@gmail.com>
2023-06-07 21:42:30 -0400

    added random titles

--- Sections/Section_39/YelpCamp/seeds/index.js
+++ Sections/Section_39/YelpCamp/seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Campground = require("../models/campground");
 const cities = require('./cities');
+const { descriptors } = require('./seedHelpers');
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -24,7 +25,8 @@ const seedDB = async() => {
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random()*1000);
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
     }
