105ace5ffe0c02dde9a3292b9e8960ccd7f75a21
Chocotapi <shioriavino@gmail.com>
2023-06-08 23:29:25 -0400

    added a post request for the new campground page

--- Sections/Section_39/YelpCamp/app.js
+++ Sections/Section_39/YelpCamp/app.js
@@ -31,6 +31,10 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
+app.post('/campgrounds', async(req, res) => {
+    res.send(req.body)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
