c90d7215c5fd03fd22d0c7f5cd25c3065a9c6dd7
Chocotapi <shioriavino@gmail.com>
2023-06-13 16:24:47 -0400

    installed a package of morgan and setup

--- Sections/Section_40/index.js
+++ Sections/Section_40/index.js
@@ -1,8 +1,17 @@
 const express = required("express");
 const app = express();
+const morgan = required("morgan");
+
+app.use('/cats', (req, res, next) => {
+    console.log("I love cats")
+})
 
 app.get('/', (req, res) => {
     res.send("Welcome to My Home Page");
+})
+
+app.get('/cats', (req, res) => {
+    res.send("Meow meow ^'w'^")
 })
 
 app.listen(3000, () => {
