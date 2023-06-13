65735b3836d3f1d1e159246a37c7bf0c5083841f
Chocotapi <shioriavino@gmail.com>
2023-06-13 16:42:27 -0400

    fixed a mistake

--- Sections/Section_40/index.js
+++ Sections/Section_40/index.js
@@ -4,10 +4,19 @@ const morgan = require("morgan");
 
 app.use(morgan('tiny'));
 //middleware runs only when you have a particular path: /cats
-app.use('/cats', (req, res, next) => {
-    console.log("I love cats");
-    next();
-})
+// app.use('/cats', (req, res, next) => {
+//     console.log("I love cats");
+//     next();
+// })
+
+//set a password to open any
+app.use((req, res, next) => {
+    const { password } = req.query;
+    if(password === 'dodgerthecat') {
+        next();
+    }
+    res.send("SORRY YOU NEED A PASSWORD");
+}) 
 
 app.get('/', (req, res) => {
     res.send("Welcome to My Home Page");
